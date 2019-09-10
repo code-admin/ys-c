de28facb277f9a3ca69db8944dbdf7b98ac7a2b0
mr.zhao <120220275@qq.com>
2019-09-10 23:39:22 +0800

    add report index and card page

--- pages/home/report/card.vue
+++ pages/home/report/card.vue
@@ -1,110 +1,109 @@
 <template>
 	<view>
 		<view class="cu-card case no-card margin-top-sm">
-			<view class="cu-item shadow" v-if="reportData.data">
+			<view class="cu-item shadow">
 				<view class="cu-bar bg-white solid-bottom">
 					<view class="action">
 						<text class="cuIcon-titles text-mauve"></text>月份：<view class="text-cut text-bold text-xl">{{reportData.reportDate}}</view>
 					</view>
 				</view>
 				<view class="padding">
+					<view class="text-lg flex justify-start margin-top-xs">
+						<view class="flex align-center justify-center flex-sub flex-direction main-block">
+							<view class="text-grey text-sm">总金额</view>
+							<view class="text-red text-bold text-price totalAmount">{{reportData.totalAmount || '--' }}</view>
+						</view>
+						<view class="flex align-center justify-center flex-sub flex-direction main-block">
+							<view class="text-grey text-sm">总收款</view>
+							<view class="text-red text-bold text-price">{{reportData.totalFee || '--' }}</view>
+						</view>
+					</view>
+					
 					<view class="text-lg flex justify-start">
-						<view class="flex align-center padding-right-xl">
-							<view class="text-gray">总重量：</view>
-							<view class="text-red text-bold">1085</view>
+						<view class="flex align-center justify-center flex-sub flex-direction main-block">
+							<view class="text-grey text-sm">总重量</view>
+							<view class="text-red text-bold totalWeight">{{reportData.totalWeight || '--' }}</view>
 						</view>
-						<view class="flex align-center">
-							<view class="text-gray ">总单价：</view>
-							<view class="text-red text-bold text-price"> 8851.4</view>
-						</view>
-					</view>
-					<view class="text-lg flex justify-start margin-top-xs">
-						<view class="flex align-center padding-right-xl">
-							<view class="text-gray ">总金额：</view>
-							<view class="text-red text-bold text-price">9599844</view>
-						</view>
-						<view class="flex align-center">
-							<view class="text-gray">总收款：</view>
-							<view class="text-red text-bold">5716699</view>
+						<view class="flex align-center justify-center flex-sub flex-direction main-block">
+							<view class="text-grey text-sm">总单价</view>
+							<view class="text-red text-bold text-price totalPrice">{{reportData.totalPrice || '--'}}</view>
 						</view>
 					</view>
-					<view class="text-lg flex justify-start margin-top-xs">
-						<view class="flex align-center padding-right-xl">
-							<view class="text-gray">总其它费用：</view>
-							<view class="text-red text-bold text-price">-29355</view>
+					
+					
+					<view class="text-lg flex justify-start margin-top-xs other-line padding-xs">
+						<view class="flex flex-sub align-center flex-direction">
+							<view class="text-red text-bold text-price">{{reportData.totalOtherFee || '--' }}</view>
+							<view class="text-grey text-xs">总其它费用</view>
 						</view>
-						<view class="flex align-center">
-							<view class="text-gray ">总退筒重量：</view>
-							<view class="text-red text-bold">27</view>
+						<view class="flex flex-sub align-center flex-direction">
+							<view class="text-red text-bold">{{reportData.totalReturnWeight || '--' }}</view>
+							<view class="text-grey text-xs">总退筒重量</view>
+						</view>
+						<view class="flex flex-sub align-center flex-direction">
+							<view class="text-red text-bold text-price">{{reportData.totalReturnFee || '--' }}</view>
+							<view class="text-grey text-xs">总退筒金额</view>
 						</view>
 					</view>
-					<view class="text-lg flex justify-start margin-top-xs">
-						<view class="flex align-center padding-right-xl">
-							<view class="text-gray ">总退筒金额：</view>
-							<view class="text-red text-bold text-price">237475</view>
+					
+					
+					<view class="cu-list grid col-4">
+						<view class="cu-item">
+							<text>&nbsp;</text>
+						</view>
+						<view class="cu-item">
+							<text class="title">重量（kg）</text>
+						</view>
+						<view class="cu-item">
+							<text class="title">单价（元 / 吨）</text>
+						</view>
+						<view class="cu-item">
+							<text class="title">金额（元）</text>
+						</view>
+						
+						<view class="cu-item">
+							<text class="title">增白</text>
+						</view>
+						<view class="cu-item">
+							<text>{{reportData.zbWeight || '--' }}</text>
+						</view>
+						<view class="cu-item">
+							<text>{{reportData.zbPrice || '--' }}</text>
+						</view>
+						<view class="cu-item">
+							<text>{{reportData.zbAmount || '--' }}</text>
+						</view>
+						
+						<view class="cu-item">
+							<text class="title">一级白</text>
+						</view>
+						<view class="cu-item">
+							<text>{{reportData.yjzbWeight || '--' }}</text>
+						</view>
+						<view class="cu-item">
+							<text>{{reportData.yjzbPrice || '--' }}</text>
+						</view>
+						<view class="cu-item">
+							<text>{{reportData.yjzbAmount || '--' }}</text>
+						</view>
+						
+						<view class="cu-item">
+							<text class="title">半透</text>
+						</view>
+						<view class="cu-item">
+							<text>{{reportData.btWeight || '--' }}</text>
+						</view>
+						<view class="cu-item">
+							<text>{{reportData.btPrice || '--' }}</text>
+						</view>
+						<view class="cu-item">
+							<text>{{reportData.btAmount || '--' }}</text>
 						</view>
 					</view>
 
-					<view class="flex justify-start solid-top margin-top-xs padding-top-xs">
-						<view class="flex align-center padding-right-xl">
-							<view class="text-gray">增白重量：</view>
-							<view class="text-grey text-bold">679</view>
-						</view>
-						<view class="flex align-center">
-							<view class="text-gray">增白单价：</view>
-							<view class="text-grey text-bold text-price">8482</view>
-						</view>
-					</view>
-					<view class="flex justify-start margin-top-xs">
-						<view class="flex align-center padding-right">
-							<view class="text-gray">增白金额：</view>
-							<view class="text-grey text-bold text-price">5761380</view>
-						</view>
-					</view>
-					<view class="flex justify-start margin-top-xs">
-						<view class="flex align-center padding-right">
-							<view class="text-gray">一级白重量：</view>
-							<view class="text-grey text-bold">161</view>
-						</view>
-						<view class="flex align-center padding-right">
-							<view class="text-gray">一级白单价：</view>
-							<view class="text-grey text-bold text-price">1085</view>
-						</view>
-					</view>
-					<view class="flex justify-start margin-top-xs">
-						<view class="flex align-center padding-right">
-							<view class="text-gray">一级白金额：</view>
-							<view class="text-grey text-bold">1595042</view>
-						</view>
-					</view>
-					<view class="flex justify-start margin-top-xs">
-						<view class="flex align-center padding-right">
-							<view class="text-gray">半透重量：</view>
-							<view class="text-grey text-bold">192</view>
-						</view>
-						<view class="flex align-center padding-right">
-							<view class="text-gray">半透单价：</view>
-							<view class="text-grey text-bold text-price">9058</view>
-						</view>
-					</view>
-					<view class="flex justify-start margin-top-xs">
-						<view class="flex align-center padding-right">
-							<view class="text-gray">半透金额：</view>
-							<view class="text-grey text-bold text-price">1740349</view>
-						</view>
-					</view>
 				</view>
 			</view>
-			<view class="cu-item shadow" v-else>
-				<view class="cu-bar bg-white solid-bottom">
-					<view class="action">
-						<text class="cuIcon-titles text-mauve"></text>月份：<view class="text-cut text-bold text-xl">{{reportData.reportDate}}</view>
-					</view>
-				</view>
-				<view class="padding text-center text-lg text-gray">
-					没有查询到当前月份到报表数据！
-				</view>
-			</view>
+			
 		</view>
 	</view>
 </template>
@@ -122,5 +121,40 @@
 </script>
 
 <style lang="scss" scoped>
-
+.main-block {
+	margin: 4upx;
+	padding: 8upx 0;
+	min-height: 100upx;
+	border-radius: 10upx;
+	border-bottom: 2upx solid #fad0d1;
+	.text-bold {
+		font-size: 55upx;
+	}
+	.totalAmount{
+		color: #1dd98d;
+	}
+	.totalWeight{
+		color: #ff9600
+	}
+	.totalPrice{
+		color: pink;
+	}
+}
+ .other-line {
+	 margin-bottom: 15upx;
+	 border-radius: 12upx;
+	 background-color: #f1f7fe;
+ }
+ .cu-list.grid>.cu-item{
+	padding: 4rpx 0;
+	width: calc((100% - 120rpx) / 3);
+	&:nth-of-type(1),&:nth-of-type(5),&:nth-of-type(9),&:nth-of-type(13){
+		width: 120rpx;
+		text-align: right;
+		padding-right: 20rpx;
+	}
+	.title{
+		color: #8799a3;
+	}
+ }
 </style>
--- pages/home/report/report.vue
+++ pages/home/report/report.vue
@@ -5,6 +5,8 @@
 			<block slot="content">报表</block>
 		</cu-custom>
 		<report-card v-for="(data,index) in dataList" :key="index" :reportData="data"></report-card>
+		<view class="text-center text-gray padding-xl" v-if="dataList && dataList.length == 0">暂无数据</view>
+		<view class="text-center text-gray padding" v-if="showLoadMore">{{loadMoreText}}</view>
 	</view>
 </template>
 
@@ -16,11 +18,41 @@
 		},
 		data() {
 			return {
-				dataList:[{reportDate:'2017-05',data:{a:1}},{reportDate:'2017-04',data:{a:1}},{reportDate:'2017-03'}]
+				queryParams: {
+					pageIndex: 1,
+					pageSize: 5,
+				},
+				total:0,
+				loadedNumber: 0,
+				showLoadMore: false,
+				loadMoreText: "加载中...",
+				dataList: []
 			}
 		},
+		onLoad() {
+			this.initData();
+		},
 		methods: {
-			
+			initData(){
+				this.loadedNumber = 0;
+				this.dataList = [];
+				this.queryParams.pageIndex = 1;
+				this.loadMoreText = "加载更多...";
+				this.showLoadMore = false;
+				this.getDataList();
+			},
+			getDataList(){
+				this.$request.post({
+					data: this.queryParams,
+					loadingTip: '加载中...',
+					url: "/report/getMonthReportList"
+				}).then(res => {
+					this.total = res.total;
+					this.loadedNumber += this.queryParams.pageSize;
+					this.dataList = this.dataList.concat(res.data) ;
+					uni.stopPullDownRefresh();
+				})
+			}
 		}
 	}
 </script>
