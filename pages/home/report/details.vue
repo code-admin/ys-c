be467ff7baf9881a0c1d206111d2c1a7ffc3d02f
mr.zhao <120220275@qq.com>
2019-10-09 09:50:27 +0800

    add product report details

--- pages/home/report/details.vue
+++ pages/home/report/details.vue
@@ -4,7 +4,74 @@
 			<block slot="backText">返回</block>
 			<block slot="content">报表详情</block>
 		</cu-custom>
-		details....
+		
+		<view class="de-content margin-bottom-xl">
+			<view class="cu-bar bg-white solid-bottom margin-top-sm">
+				<view class="action">
+					<text class="cuIcon-titles text-orange"></text> {{itemdata.productName || "产品信息" }}
+				</view>
+				<view class="margin-right text-sm text-gray">
+					{{beginDate}} ~ {{endDate}}
+				</view>
+			</view>
+			<view class="cu-item flex">
+				<view class="title text-grey padding-sm">要&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;求：</view>
+				<view class="padding-sm">{{itemdata.requirement || ''}}</view>
+			</view>
+			<view class="cu-item flex">
+				<view class="title text-grey padding-sm">宽&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;度：</view>
+				<view class="padding-sm">{{itemdata.productWidth|| ''}} (cm)</view>
+			</view>
+			<view class="cu-item flex">
+				<view class="title text-grey padding-sm">克&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;重：</view>
+				<view class="padding-sm">{{itemdata.productWeight || ''}} (g)</view>
+			</view>
+			<view class="cu-item flex">
+				<view class="title text-grey padding-sm">长&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;度：</view>
+				<view class="padding-sm">{{itemdata.productLength|| ''}} (cm)</view>
+			</view>
+			<view class="cu-item flex">
+				<view class="title text-grey padding-sm">个&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;数：</view>
+				<view class="padding-sm">{{itemdata.goodsNumber || ''}}</view>
+			</view>
+			<view class="cu-item flex">
+				<view class="title text-grey padding-sm">退筒个数：</view>
+				<view class="padding-sm">{{itemdata.returnNumber|| ''}}</view>
+			</view>
+			<view class="cu-item flex">
+				<view class="title text-grey padding-sm">退筒重量：</view>
+				<view class="padding-sm">{{itemdata.weight|| ''}}</view>
+			</view>
+			<view class="cu-item flex">
+				<view class="title text-grey padding-sm">退筒金额：</view>
+				<view class="padding-sm">{{itemdata.returnAmount|| ''}}</view>
+			</view>
+			<view class="cu-item flex">
+				<view class="title text-grey padding-sm">其&nbsp;它&nbsp;&nbsp;款：</view>
+				<view class="padding-sm">{{itemdata.otherAmount|| ''}}</view>
+			</view>
+			<view class="cu-item flex">
+				<view class="title text-grey padding-sm">重&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;量：</view>
+				<view class="padding-sm">{{itemdata.totalWeight|| ''}}</view>
+			</view>
+			<view class="cu-item flex">
+				<view class="title text-grey padding-sm">单&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;价：</view>
+				<view class="padding-sm">{{itemdata.price|| ''}}</view>
+			</view>
+			<view class="cu-item flex">
+				<view class="title text-grey padding-sm">金&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;额：</view>
+				<view class="padding-sm">{{itemdata.totalAmount|| ''}}</view>
+			</view>
+			<view class="cu-item flex">
+				<view class="title text-grey padding-sm">已&nbsp;结&nbsp;&nbsp;款：</view>
+				<view class="padding-sm">{{itemdata.paidAmount|| ''}}</view>
+			</view>
+			<view class="cu-item flex">
+				<view class="title text-grey padding-sm">余&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;款：</view>
+				<view class="padding-sm">{{itemdata.balanceAmount|| ''}}</view>
+			</view>
+		</view>
+		
 	</view>
 </template>
 
@@ -41,5 +108,21 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.de-content{
+		background-color: white;
+	}
+	.cu-item {
+		.text-grey {
+			display: flex;
+			/** flex-basis: 65px; */
+			white-space: nowrap;
+			justify-content: space-between;
+			&+.padding-right-xl,&+.padding-right-xs{
+				min-width: 75px;
+			}
+		}
+	}
+
 </style>
+
